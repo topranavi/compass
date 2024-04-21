9ca764029fa708b60c29ef49010b93cad5fe8d71
topranavi <167685853+topranavi@users.noreply.github.com>
2024-04-22 00:14:25 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,19 @@ basic.forever(function () {
     degrees = input.compassHeading()
     if (degrees < 45) {
         basic.showString("N")
+        serial.writeLine("" + (degrees))
     } else if (degrees < 135) {
         basic.showString("E")
+        serial.writeLine("" + (degrees))
     } else if (degrees < 225) {
         basic.showString("S")
+        serial.writeLine("" + (degrees))
     } else if (degrees < 315) {
         basic.showString("W")
+        serial.writeLine("Degree")
+        serial.writeLine("" + (degrees))
     } else {
         basic.showString("N")
+        serial.writeLine("" + (degrees))
     }
 })
